449ccce012992b7661ef7d2d8252424f385831f2
Medina-15 <alisonmmmr@gmail.com>
2026-03-20 21:57:00 -0400

    Add files via upload

---
+++ CalculadoraPrueva/src/calculadoraprueva/Calculadora.java
@@ -0,0 +1,13 @@
+
+package calculadoraprueva;
+
+
+class Calculadora {
+    public double sumar (Double valor1, Double valor2 ){
+       return valor1 + valor2;
+}
+    public double Restar (Double valor1, Double valor2 ){
+       return valor1 - valor2;
+}
+
+}
---
+++ CalculadoraPrueva/src/calculadoraprueva/CalculadoraPrueva.java
@@ -0,0 +1,20 @@
+
+package calculadoraprueva;
+
+
+public class CalculadoraPrueva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora cal = new Calculadora();
+        Double resultado = cal.sumar(3.5, 6.5);
+        System.out.println("resultado" + resultado);
+    }
+
+    public void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
